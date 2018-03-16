7413f8c79ad5b6deebc002ffacd2cdb8b7426363
Reapy13 <hugo.fauchery@gmail.com>
2018-03-16 09:42:24 +0100

    Adds firebase and read the record of the player

--- app/src/main/java/m2dl/com/howhigh/StartActivity.java
+++ app/src/main/java/m2dl/com/howhigh/StartActivity.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.logging.Logger;
+
 public class StartActivity extends AppCompatActivity {
 
     private Button playButton;
@@ -14,6 +21,10 @@ public class StartActivity extends AppCompatActivity {
     private Button quitButton;
 
     private TextView record;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    private static final Logger LOGGER = Logger.getLogger(StartActivity.class.getName());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,20 @@ public class StartActivity extends AppCompatActivity {
         quitButton = (Button) findViewById(R.id.quit);
         record = (TextView) findViewById(R.id.record);
 
-        record.setText("Pas de record");
+        database.getReference().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                LOGGER.info("On entre dedans");
+                String recordPlayer = dataSnapshot.child("Record").getValue(Long.class).toString();
+                LOGGER.info("Contenu du record : " + recordPlayer);
+                record.setText("Record : " + recordPlayer);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void launchGame(View view) {
