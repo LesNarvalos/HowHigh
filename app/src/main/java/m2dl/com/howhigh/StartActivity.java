53658a17439d51f82b0d6b05689e4a182838ece9
Reapy13 <hugo.fauchery@gmail.com>
2018-03-16 09:21:08 +0100

    Adding buttons "Play" & "Quit" and the record text view

--- app/src/main/java/m2dl/com/howhigh/StartActivity.java
+++ app/src/main/java/m2dl/com/howhigh/StartActivity.java
@@ -1,13 +1,38 @@
 package m2dl.com.howhigh;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button playButton;
+
+    private Button quitButton;
+
+    private TextView record;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        playButton = (Button) findViewById(R.id.play);
+        quitButton = (Button) findViewById(R.id.quit);
+        record = (TextView) findViewById(R.id.record);
+
+        record.setText("Pas de record");
+    }
+
+    public void launchGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void quit(View view) {
+        this.finish();
     }
 }
